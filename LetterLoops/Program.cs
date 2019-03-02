9dd8dfeb008045b325282779d6630c80618bd7cc
unknown <c.chase.hamby@gmail.com>
2019-03-02 10:41:06 -0600

    works!

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LetterLoops
 {
@@ -6,7 +7,23 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TEST!");
+            Console.WriteLine("Please input a string!");
+            var input = Console.ReadLine().ToLower();
+            var loopCount = 0;
+            var myCharacters = new List<string>();
+
+            foreach (var character in input)
+            {
+
+                var letterString = character.ToString().ToUpper();
+                letterString += new string(character, loopCount);
+
+                myCharacters.Add(letterString);
+
+                loopCount++;
+            }
+            Console.WriteLine(string.Join('-', myCharacters));
+            Console.ReadKey();
         }
     }
 }
